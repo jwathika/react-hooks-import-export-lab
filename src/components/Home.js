d461e1e1b4aeea9f571b0202382b9e2935ed792e
jwathika <wathika02@gmail.com>
2024-04-24 21:11:43 +0300

    Refactor code formatting in NavBar.js, user.js, index.js, App.js, Home.js, and About.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
-import React from "react";
-
+import React from 'react';
+import { username, city } from '../data/user';
+console.log(username);
 function Home() {
-  return (
-    <div id="home">
-      <h1>
-        {username} is a Web Developer from {city}
-      </h1>
-    </div>
-  );
+	return (
+		<div id='home'>
+			<h1>
+				{username} is a Web Developer from {city}
+			</h1>
+		</div>
+	);
 }
+export default Home;
